f802b0d3a83c8eb4dd388511e48f6b4cd48dd643
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-01 18:03:03 +0200

    Solution for first task

---
+++ 01.dom_selector/src/selectorTs.ts
@@ -0,0 +1,6 @@
+function domSelector(selectors: string) {
+  if ((/[^A-Za-z0-9.#\-\s]/.test(selectors)) && !selectors) {
+    throw new Error('Selector contains faulty characters');
+  }
+  return Array.from(document.querySelectorAll(selectors));
+}
