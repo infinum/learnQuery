b1fd22603a8628de49ecfdf548eb56528bd4e504
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-01 21:55:16 +0200

    Refactor selectorTs to use arrow function

--- 01.dom_selector/src/selectorTs.ts
+++ 01.dom_selector/src/selectorTs.ts
@@ -1,4 +1,4 @@
-function domSelector(selectors: string) {
+let domSelector = (selectors: string) => {
   if ((/[^A-Za-z0-9.#\-\s]/.test(selectors)) && !selectors) {
     throw new Error('Selector contains faulty characters');
   }
