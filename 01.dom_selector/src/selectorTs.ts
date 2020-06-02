1272270686d80948660a6a193b568703eee5a4bf
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-02 09:29:48 +0200

    Replace let with const

--- 01.dom_selector/src/selectorTs.ts
+++ 01.dom_selector/src/selectorTs.ts
@@ -1,4 +1,4 @@
-let domSelector = (selectors: string) => {
+const domSelector = (selectors: string) => {
   if ((/[^A-Za-z0-9.#\-\s]/.test(selectors)) && !selectors) {
     throw new Error('Selector contains faulty characters');
   }
