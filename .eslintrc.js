e974daf255529225fe73a042e0c8dba555e43e2e
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-03 14:27:06 +0200

    Add Airbnb Eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "root": true,
+  "extends": ['airbnb-typescript'],
+  "rules": {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error"
+    ]
+  }
+};
