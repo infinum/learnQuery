74b88d181a5690f9c4b18bd5944c22e5a81cef19
Sven Kolarić <sven.kolaric@intellexi.hr>
2020-06-03 15:35:58 +0200

    Fix lint errors in selectorTs

--- 01.dom_selector/src/selectorTs.ts
+++ 01.dom_selector/src/selectorTs.ts
@@ -1,6 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const domSelector = (selectors: string) => {
   if ((/[^A-Za-z0-9.#\-\s]/.test(selectors)) && !selectors) {
     throw new Error('Selector contains faulty characters');
   }
   return Array.from(document.querySelectorAll(selectors));
-}
+};
