dc64f60be2580bff50c30fb2564165888c895252
Dominik <dominik.7@live.com>
2023-09-28 00:13:44 +0200

    Solved first exercise

--- 01.dom_selector/src/selector.js
+++ 01.dom_selector/src/selector.js
@@ -2,4 +2,14 @@ var domSelector = function(selectors) {
   'use strict';
 
   //code goes here
+  return document.querySelectorAll(selectors).length === 0 ? new Array() : Array.from(document.querySelectorAll(selectors))
 };
+
+/*
+prvotno rjesenje
+let nodeList = document.querySelectorAll(selectors);
+if (nodeList.length === 0) {
+  return new Array();
+}
+else return Array.from(nodeList);
+*/ 
