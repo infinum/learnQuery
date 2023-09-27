c426d683a7ffcdbb817f105c5c91ed5266fac78b
Dominik <dominik.7@live.com>
2023-09-28 00:11:59 +0200

    Partially solved second exercise

--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -2,4 +2,14 @@ function cssProp() {
   'use strict';
 
   //code goes here
+  switch (arguments.length) {
+    case 2:
+      if (typeof arguments[1] ===  'object'){
+        let entries = Object.entries(arguments[1]);
+        entries.forEach(entry => arguments[0].setAttribute('style', `${entry[0]} : ${entry[1]}`))        
+      }
+      return arguments[0].style[`${arguments[1]}`];
+    case 3:
+      return arguments[0].setAttribute('style', `${arguments[1]} : ${arguments[2]}`);
+  }
 }
