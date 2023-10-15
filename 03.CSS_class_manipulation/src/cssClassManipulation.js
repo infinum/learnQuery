4a9d81da727d8b7090712b4550a341902baba05b
Dominik <dominik.7@live.com>
2023-10-15 13:28:40 +0200

    Solve 03_class_manipulation

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -2,4 +2,21 @@ var cssClass = (function() {
   'use strict';
 
   // code goes here
+  function add (htmlElement, className) {
+    htmlElement.classList.add(className);
+  }
+
+  function remove (htmlElement, className) {
+    htmlElement.classList.remove(className);
+  }
+
+  function toggle (htmlElement, className) {
+    htmlElement.classList.toggle(className);
+  }
+
+  function has (htmlElement, className) {
+    return htmlElement.classList.contains(className);
+  }
+
+  return {add, remove, toggle, has}
 })();
